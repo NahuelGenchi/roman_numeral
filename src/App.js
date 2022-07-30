f3593726aa52210219dc6238ef0261fec5aa8568
Nahuel Genchi <nahuelgenchi@gmail.com>
2022-07-30 13:55:18 -0300

    header done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./index.scss";
 
+import { Header, Main, Footer } from "./container";
+
 function App() {
   return (
-    <></>
+    <div className="body-container">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
   );
 }
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,8 +1,12 @@
 import "./Header.scss";
 
-const Header = function() {
+const Header = function(props) {
   return (
-    <></>
+    <header className="header">
+      <h1>Roman Numeral Converter</h1>
+      <p>Use this Roman numeral converter to convert arabic numerals into Roman numerals.</p>
+      <p>Roman numerals are a number system developed in ancient Rome where letters represent numbers. The modern use of Roman numerals involves the letters I, V, X, L, C, D, and M.</p>
+    </header>
   );
 };
 
