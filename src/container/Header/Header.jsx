20a26c9ec6b9806efa30cf329bb4c5cfe25efacb
Nahuel Genchi <nahuelgenchi@gmail.com>
2022-07-31 22:22:23 -0300

    logic done

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.scss";
 const Header = function(props) {
   return (
     <header className="header">
-      <h1>Roman Numeral Converter</h1>
+      <h1>Roman Converter</h1>
       <p>Use this Roman numeral converter to convert arabic numerals into Roman numerals.</p>
       <p>Roman numerals are a number system developed in ancient Rome where letters represent numbers. The modern use of Roman numerals involves the letters I, V, X, L, C, D, and M.</p>
     </header>
--- src/container/Main/Main.jsx
+++ src/container/Main/Main.jsx
@@ -1,8 +1,62 @@
+import {useRef, useState} from "react";
 import "./Main.scss";
 
 const Main = function() {
+  const [num, setNum] = useState('');
+  const [message, setMessage] = useState('');
+  const inputRef = useRef(null);
+  const min = 1;
+  const max = 4999;
+
+  const handleNumChange = function(event) {
+    const value = Math.max(min, Math.min(max, Number(event.target.value)));
+    setNum(value);
+  }
+  function romanize(num) {
+    let lookup = {
+      M:1000,
+      CM:900,
+      D:500,
+      CD:400,
+      C:100,
+      XC:90,
+      L:50,
+      XL:40,
+      X:10,
+      IX:9,
+      V:5,
+      IV:4,
+      I:1
+    };
+    let roman = "";
+    let i;
+    for (i in lookup) {
+      while (num >= lookup[i]) {
+        roman += i;
+        num -= lookup[i];
+      };
+    };
+    return roman;
+  }
+
+  function handleClick() {
+    let myInput = inputRef.current.value;
+    setMessage(romanize(myInput));
+    setNum("");
+  };
+
   return (
-    <></>
+    <main className="main">
+      <div className="input-container">
+        <input ref={inputRef} type="number" value={num} onChange={handleNumChange} placeholder="Type a number.."/>
+        <button onClick={handleClick}><ion-icon name="repeat"></ion-icon></button>
+      </div>
+      <div className="output-container">
+        <span>
+          {message}
+        </span>
+      </div>
+    </main>
   );
 };
 
